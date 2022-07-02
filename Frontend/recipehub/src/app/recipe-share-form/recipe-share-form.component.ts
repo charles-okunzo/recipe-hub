2041ccfea1fa7cca2810b7f53b2bbe27fb31d618
charles okunzo <okunzopeter01@gmail.com>
2022-07-02 12:22:58 +0300

    feature add recipe shairing form component

---
+++ Frontend/recipehub/src/app/recipe-share-form/recipe-share-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-share-form',
+  templateUrl: './recipe-share-form.component.html',
+  styleUrls: ['./recipe-share-form.component.css']
+})
+export class RecipeShareFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
