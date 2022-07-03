9d77afdaea1f673f2d7cc371573a7a4bf26c7ee3
charles okunzo <okunzopeter01@gmail.com>
2022-07-03 22:00:42 +0300

    feature --modify Recipe model

--- Backend/recipe_api/models.py
+++ Backend/recipe_api/models.py
@@ -3,19 +3,27 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 # Create your models here.
-
+DISH_TYPE_CHOICES = {
+    ('Main Dish', 'Main Dish'),
+    ('Side Dish', 'Side Dish'),
+    ('Appetizer', 'Appetizer'),
+    ('Soup', 'Soup'),
+    ('Salad', 'Salad'),
+    ('Dessert', 'Dessert'),
+    ('Drink', 'Drink')
+}
 
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=100)
-    dish_type = models.CharField(max_length=100)
-    prep_time_mins = models.PositiveIntegerField(max_length=5)
-    no_of_servings = models.PositiveIntegerField(max_length=5)
-    cooking_time_mins = models.PositiveIntegerField(max_length=5)
-    image = CloudinaryField(upload_to = 'recipe_images')
+    dish_type = models.CharField(max_length=100, choices=DISH_TYPE_CHOICES)
+    prep_time_mins = models.PositiveIntegerField()
+    no_of_servings = models.PositiveIntegerField()
+    cooking_time_mins = models.PositiveIntegerField()
+    image = CloudinaryField('recipe_images')
     ingredients = models.TextField()
     instructions = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipe_owner')
-    ratings = models.PositiveIntegerField(max_length=1)
+    ratings = models.PositiveIntegerField()
     country = models.CharField(max_length=100)
     bookmarked = models.BooleanField(default=False)
