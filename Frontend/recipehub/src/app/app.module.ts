41af1d68ff91b00996fd26b5c63f2dd00318da65
carol-profile <carolgitonga93@gmail.com>
2022-07-04 10:51:40 +0300

    create db.json file

--- Frontend/recipehub/src/app/app.module.ts
+++ Frontend/recipehub/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import {HttpClientModule} from '@angular/common/http';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    // RouterModule,  
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/recipehub/src/app/signup/signup.component.ts
+++ Frontend/recipehub/src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder} from '@angular/forms'
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  
+  public signupForm !:FormGroup;
+  constructor(private formBuilder : FormBuilder, private http : HttpClient ,private router:Router) { }
 
   ngOnInit(): void {
-  }
+    this.signupForm = this.formBuilder.group({
+      Username:[''],
+      Email:[''],
+      Password:[''],
+      Confirmation:[''],
+    })
 
-}
+  }
+  signUp(){
+    this.http.post<any>('http://localhost:3000/signupUsers',this.signupForm.value)
+    .subscribe(res =>{
+      alert('Signup Successful');
+      this.signupForm.reset();
+      this.router.navigate(['login']);
+    })
+    }
+    
+
+    }
+
+
+
+
