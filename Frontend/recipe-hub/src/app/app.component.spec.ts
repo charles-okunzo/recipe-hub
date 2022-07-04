33c2e5f36c9aee623a898e1cb0a10397582c1c41
Maltilda Nyaboke <98155106+Maltilda-Nyaboke@users.noreply.github.com>
2022-07-04 03:45:11 +0300

    Update app.component.specs.ts

--- Frontend/recipe-hub/src/app/app.component.spec.ts
+++ Frontend/recipe-hub/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'recipe-hub'`, () => {
+  it(`should have as title 'recipehub'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipe-hub');
+    expect(app.title).toEqual('recipehub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('recipe-hub app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('recipehub app is running!');
   });
 });
