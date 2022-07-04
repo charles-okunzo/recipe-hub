8c8840be5463546cdff012cddb96d607b8bc8e50
charles okunzo <okunzopeter01@gmail.com>
2022-07-04 20:08:00 +0300

    style --add route links

--- Frontend/recipehub/src/app/app-routing.module.ts
+++ Frontend/recipehub/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'', component:LandingPageComponent},
   {path:'login', component:LoginComponent},
-  {path:'signup', component:SignupComponent}
+  {path:'signup', component:SignupComponent},
+  // {path:'',redirectTo:'',pathMatch:'full'}
 ];
 
 
