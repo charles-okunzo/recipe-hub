615b2928663b8fa412eccea6678118fae87f24d5
charles okunzo <okunzopeter01@gmail.com>
2022-07-04 14:58:49 +0300

    rfeature --add APi viewsets

--- Backend/recipe_api/views.py
+++ Backend/recipe_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, routers
+from django.contrib.auth.models import User
+from recipe_api.models import Recipe
+from Backend.recipe_api.serializers import RecipeSerializer, UserSerializer
 
 # Create your views here.
+
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User
+    serializer_class = UserSerializer
+
+
+class RecipeViewset(viewsets.ModelViewSet):
+    queryset = Recipe
+    serializer_class = RecipeSerializer
