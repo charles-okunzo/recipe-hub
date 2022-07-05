d296b7a7382f7331c85422c6215b5173c8a889bd
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-07-05 10:48:12 +0300

    test:git branches

--- Backend/recipe_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from recipe_api.models import Recipe
-
-# Register your models here.
-
-admin.site.register(Recipe)
--- Backend/recipe_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecipeApiConfig(AppConfig):
-    name = 'recipe_api'
