6d8d35e19648956b25a1ac21efe1e4b82e6eab73
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-07-05 08:35:24 +0300

    feature:create recipe-detail component

--- Frontend/recipe/src/app/app.module.ts
+++ Frontend/recipe/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
