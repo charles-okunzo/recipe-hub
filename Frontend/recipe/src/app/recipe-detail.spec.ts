3d1ea552ff97756257ff63b8ffe4ae7c3f3fdb0d
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-07-05 08:33:52 +0300

    style:delete class recipe detail

--- Frontend/recipe/src/app/recipe-detail.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RecipeDetail } from './recipe-detail';
-
-describe('RecipeDetail', () => {
-  it('should create an instance', () => {
-    expect(new RecipeDetail()).toBeTruthy();
-  });
-});
