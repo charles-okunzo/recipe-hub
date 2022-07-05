bd8d3d40c8e10b73ff4227610dff63c98de7ff39
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-07-05 08:31:20 +0300

    create class recipe detail

---
+++ Frontend/recipe/src/app/recipe-detail.spec.ts
@@ -0,0 +1,7 @@
+import { RecipeDetail } from './recipe-detail';
+
+describe('RecipeDetail', () => {
+  it('should create an instance', () => {
+    expect(new RecipeDetail()).toBeTruthy();
+  });
+});
