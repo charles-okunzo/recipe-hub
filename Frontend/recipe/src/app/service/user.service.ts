7e921426885d4bab47359d1efd57277f87c36081
charles okunzo <okunzopeter01@gmail.com>
2022-07-06 21:18:12 +0300

    feature --add user servive

---
+++ Frontend/recipe/src/app/service/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  BASEURL='https://rec1pe-api.herokuapp.com/api/users/'
+
+  registerUser(userData): Observable<any> {
+    return this.http.post(this.BASEURL, userData);
+  }
+}
--- Frontend/recipe/src/app/signup/signup.component.ts
+++ Frontend/recipe/src/app/signup/signup.component.ts
@@ -12,7 +12,7 @@ export class SignupComponent implements OnInit {
   
   public signupForm !:FormGroup;
   constructor(private formBuilder : FormBuilder, private http : HttpClient ,private router:Router) { }
-
+// register;
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
       Username:[''],
@@ -21,19 +21,30 @@ export class SignupComponent implements OnInit {
       Confirmation:[''],
     })
 
+  // }
+  // signUp(){
+  //   this.http.post<any>('http://localhost:3000/signupUsers',this.signupForm.value)
+  //   .subscribe(res =>{
+  //     alert('Signup Successful');
+  //     this.signupForm.reset();
+  //     this.router.navigate(['login']);
+  //   })
+
+
+    // this.register  = {
+    //   username : '',
+    //   email: '',
+    //   password : '' 
+    // }
+
+
+    }
+
+
+    registerUser(){
+      console.log(this.signupForm.value)
+    }
+
+
   }
-  signUp(){
-    this.http.post<any>('http://localhost:3000/signupUsers',this.signupForm.value)
-    .subscribe(res =>{
-      alert('Signup Successful');
-      this.signupForm.reset();
-      this.router.navigate(['login']);
-    })
-    }
-    
-
-    }
-
-
-
 
