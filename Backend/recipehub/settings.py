9e679e10c8bcb213bb89e309860dd6fb0ddfcaaa
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-07-06 16:43:47 +0300

    patch:run django

--- Backend/recipehub/settings.py
+++ Backend/recipehub/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipe',
     'recipe_api',
     'rest_framework',
     "corsheaders",
--- Backend/recipehub/urls.py
+++ Backend/recipehub/urls.py
@@ -18,10 +18,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('api/', include('recipe_api.urls')),
-    path('auth/', include('recipe_api.auth_api.urls'))
-=======
+    path('auth/', include('recipe_api.auth_api.urls')),
     path('api/', include('recipe_api.urls'))
->>>>>>> boni
+
 ]
