0ee8ac1da0c2a22b272e525d455e33381adef4d7
charles okunzo <okunzopeter01@gmail.com>
2022-07-11 20:25:29 +0300

    feature --add bookmark and rating path

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from rest_framework import routers
+from recipe_api.serializers import RatingSerializer
 
-from recipe_api.views import ProfileViewset, RecipeViewset, UserViewset
+from recipe_api.views import BookmarkViewSet, ProfileViewset, RecipeViewset, UserViewset
 
 routes = routers.DefaultRouter()
 
 routes.register('users', UserViewset)
 routes.register('recipes', RecipeViewset)
 routes.register('profile', ProfileViewset)
+routes.register('bookmarks', BookmarkViewSet)
+routes.register('ratings', RatingSerializer)
 
 urlpatterns = [
     # path()
