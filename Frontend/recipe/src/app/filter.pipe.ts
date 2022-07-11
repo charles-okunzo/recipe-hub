d067db5f444c31bb39cfb0337dcc7fc827b88ff5
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-07-11 04:13:13 +0300

    test:search functionality

--- Frontend/recipe/src/app/filter.pipe.ts
+++ Frontend/recipe/src/app/filter.pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(items: any[], searchText: string): any[] {
+    if (!items) {
+      return [];
+    }
+    if (!searchText) {
+      return items;
+    }
+    searchText = searchText.toLocaleLowerCase();
 
+    return items.filter(it => {
+      return it.toLocaleLowerCase().includes(searchText);
+    });
+  }
 }
--- Frontend/recipe/src/app/recipe.ts
+++ Frontend/recipe/src/app/recipe.ts
@@ -1,3 +1,15 @@
 export class Recipe {
-    name!: string;
+    id! : number;
+    recipe_name!: string;
+    dish_type!: string;
+    prep_time_mins!:string;
+    no_of_servings!:string;
+    cooking_time_mins!:string;
+    ingredients!:string;
+    instructions!: string;
+    date_created!: Date;
+    posted_by!: string;
+    ratings!: number;
+    country!: string;
+    bookmarked!: boolean;
 }
