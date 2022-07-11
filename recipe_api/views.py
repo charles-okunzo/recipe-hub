876071db82a976939aeae36c76b75bcef2d8de7f
charles okunzo <okunzopeter01@gmail.com>
2022-07-11 12:45:52 +0300

    feat --add perform_create,destroy and perform update methods on Viewsests

--- recipe_api/views.py
+++ recipe_api/views.py
@@ -5,6 +5,7 @@ from recipe_api.models import Profile, Recipe
 from recipe_api.serializers import ProfileSerializer, RecipeSerializer, UserSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -21,8 +22,33 @@ class RecipeViewset(viewsets.ModelViewSet):
     # permission_classes = (IsAuthenticatedOrReadOnly,)
     # authentication_classes = (TokenAuthentication,)
 
+    #add more information before creating a user
+    def perform_create(self, serializer):
+        
+        if self.request.user.is_authenticated:
+            serializer.save(posted_by = self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        recipe = self.get_object()
+
+        user = request.user
+        if recipe.posted_by == user:
+            recipe.delete()
+            return Response(data='delete success')
+        return Response(data='permission denied')
+        
+    #add more info i.e user before updating
+    def perform_update(self, serializer):
+        if self.request.user.is_authenticated:
+            serializer.save(posted_by = self.request.user)
+
 class ProfileViewset(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     # permission_classes = (IsAuthenticatedOrReadOnly,)
     # authentication_classes = (TokenAuthentication,)
+
+    def perform_update(self, serializer):
+        if self.request.user.is_authenticated:
+            serializer.save(posted_by = self.request.user)
+
