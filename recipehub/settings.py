049b7e9f18d98b7d3b130cf219c90780ea789725
charles okunzo <okunzopeter01@gmail.com>
2022-07-12 11:27:14 +0300

    feat --modify default profile image

--- recipehub/settings.py
+++ recipehub/settings.py
@@ -154,7 +154,7 @@ REST_USE_JWT = True
 #simple Json web tokens custom settings
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
